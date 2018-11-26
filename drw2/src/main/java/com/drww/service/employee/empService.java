75bc490f7cb379a8aca701650bf87f760de2bed7
orangeAndoranges <m15638903307@163.com>
2018-11-26 10:11:27 +0800

    测试4

---
+++ drw2/src/main/java/com/drww/service/employee/empService.java
@@ -0,0 +1,15 @@
+package com.drww.service.employee;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * className:empService
+ * descriptive:
+ * author:万中华
+ * createTime:2018-11-24 19:17
+ */
+public interface empService {
+
+    List<Map> getList(Map map);
+}
